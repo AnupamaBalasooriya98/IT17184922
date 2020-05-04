ab67e2189565939ee51bc076484ff4392e81dcea
Anupama <anubalasooriya2017@gmail.com>
2020-05-05 04:04:14 +0530

    Created insert doctor method

--- Doctors/src/model/Doctor.java
+++ Doctors/src/model/Doctor.java
@@ -2,8 +2,10 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Doctor {
+	// Database connection
 	public Connection connection() {
 		Connection con = null;
 		
@@ -17,4 +19,38 @@ public class Doctor {
 		
 		return con;
 	}
+	
+	// Insert doctor details
+	public String Insert(String dname, String mail, int pno, String spec) {
+		String output = "";
+		
+		try {
+			Connection con = connection();
+			
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			
+			String qry = "INSERT INTO doctors VALUES (?, ?, ?, ?)";
+			
+			PreparedStatement pstmt = con.prepareStatement(qry);
+			
+			pstmt.setString(1, dname);
+			pstmt.setString(2, mail);
+			pstmt.setInt(3, pno);
+			pstmt.setString(4, spec);
+			
+			con.close();
+			
+			output = "Inserted";
+		} catch (Exception e) {
+			// TODO: handle exception
+			output = "Error";
+			System.out.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	
 }
