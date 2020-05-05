19de6d32417a8526a282a3d06b0721843b2cdc8b
Anupama <anubalasooriya2017@gmail.com>
2020-05-06 04:17:17 +0530

    Created ADD button function

---
+++ Doctors/WebContent/Components/main.js
@@ -0,0 +1,57 @@
+
+// add button
+$(document).ready(function() {
+	$("#alertSuccess").hide;
+	$("#alertError").hide;
+});
+
+$(document).on("click", "#btnAdd", function(event) {
+	// clear status messages
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// form validation
+	var status = validateDoctorsForm();
+	
+	// if not valid
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	//if valid
+	var doctor = getDoctorCard($("#name").val().trim(),
+			$("#phone").val());
+	
+	$("#colDoctors").append(doctor);
+	
+	$("#alertSuccess").text("Saved successfully");
+	$("#alertSuccess").show();
+	
+	$("#formDoctors")[0].reset();	
+});
+
+function validateDoctorsForm() {
+	// name
+	if ($("#name").val().trim() == "") {
+		return "Insert doctor name";
+	}
+	
+	// phone number
+	if ($("#phone").val().trim() < 10 || $("#phone").val().trim() > 10) {
+		return "Enter valid phone number";
+	}
+	
+	else {
+		return true;
+	}
+}
+
+function getDoctorCard(name, phone) {
+	
+}
+
+// delete button
